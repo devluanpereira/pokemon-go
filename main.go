7daace83e056522a00f1de40817d89a350a96089
devluanpereira <luan23107@gmail.com>
2024-08-29 12:07:59 -0300

    devluanpereira

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"text/template"
 )
 
 // Estrutura para armazenar os dados do Pokémon
@@ -31,4 +33,37 @@ func fetchPokemon(name string) (*Pokemon, error) {
 	}
 
 	return &pokemon, nil
+}
+
+// Função para renderizar a pagina inicial
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/index.html"))
+	tmpl.Execute(w, nil)
+}
+
+// Função para rendenrizar o card do pokemon
+
+func pokemonHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+	if name == "" {
+		http.Error(w, "Faltou o nome do Pokemon.", http.StatusBadRequest)
+		return
+	}
+
+	pokemon, err := fetchPokemon(name)
+	if err != nil {
+		http.Error(w, "Erro ao buscar o Pokemon.", http.StatusInternalServerError)
+		return
+	}
+
+	tmpl := template.Must(template.ParseFiles("templates/pokemon.html"))
+	tmpl.Execute(w, pokemon)
+}
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/pokemon", pokemonHandler)
+	fmt.Println("Servidor rodando na porta :8080...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
